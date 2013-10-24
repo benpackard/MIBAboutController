4e35f26c1d0ed634a0f230a15f77097281ab1f30
Ben Packard <ben.packard@gmail.com>
2013-10-24 16:30:39 -0400

    Initial Commit

---
+++ MIBAboutController/MIBAboutController.h
@@ -0,0 +1,13 @@
+//
+//  MIBAboutController.h
+//  MIBAboutController
+//
+//  Created by Ben Packard on 10/24/13.
+//  Copyright (c) 2013 Ben Packard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MIBAboutController : NSObject
+
+@end
---
+++ MIBAboutController/MIBAboutController.m
@@ -0,0 +1,13 @@
+//
+//  MIBAboutController.m
+//  MIBAboutController
+//
+//  Created by Ben Packard on 10/24/13.
+//  Copyright (c) 2013 Ben Packard. All rights reserved.
+//
+
+#import "MIBAboutController.h"
+
+@implementation MIBAboutController
+
+@end
